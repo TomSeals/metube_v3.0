e19f9ad6abb8a1e53d76d57d3e079db78692c79a
Thomas Seals <tjseals@gmail.com>
2013-12-03 15:58:46 -0600

    Metube 3.0 up and running

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,10 +3,12 @@ class VideosController < ApplicationController
   def show
     id = params[:id_from_user]
     @video = Video.find(id)
+    @first = Video.first
+    @last = Video.last
+    
   end
   
   def show_all
     @videos = Video.all
   end
-
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,32 @@
 class Video < ActiveRecord::Base
+
+	def snippet
+		self.description.truncate(100)
+	end
+
+	def rating
+		video_rating = rand(1..5)
+	end
+
+	def next
+		# Video.where(["id >  ", id]).first
+
+		if self.id < Video.last.id
+			self.id + 1
+		else 
+			self.id = Video.first.id
+		end
+	end
+
+	def previous
+		if self.id > Video.first.id
+			self.id - 1
+		else 
+			self.id = Video.last.id
+		end
+	end
+
+		# counter = 0
+		# videos = Video.all
+		# if counter < videos.length
 end
