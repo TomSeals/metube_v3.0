f05c7b18703bfc877d27eb9daba18ed5958366c2
Thomas Seals <tjseals@gmail.com>
2013-12-05 13:26:02 -0600

    Connected Videos and Users databases'

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'hirb'
 
+gem 'bcrypt-ruby', '~> 3.1.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,30 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @video.destroy
+    redirect_to root_path
   end
 
   private
