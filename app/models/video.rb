13a6cd627034a423717458ab5c34676f527322e4
Thomas Seals <tjseals@gmail.com>
2013-12-05 15:18:30 -0600

    all tests pass

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,14 @@ class Video < ActiveRecord::Base
   belongs_to :user
   has_many :ratings
   validates :user_id, :title, :youtube_id, presence: true
+
+  def avg_rating
+  	total = 0.0
+  	rating_ary = self.ratings.to_a
+	  if rating_ary.length > 0
+	  	Rating.average(:value)
+	  else 
+	  		return 0
+  	end
+  end
 end
